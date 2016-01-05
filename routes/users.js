6f76ecb7f7e706eb2a4286271809482fa6305518
Jon Harlem <jtharlem@gmail.com>
2016-01-05 13:46:41 -0800

    added redirect for view all users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/:user/profile', function(req, res, next) {
+  res.render('users/profile', {profileUser: req.params.user})
+});
+
+router.get('/', ensureLoggedInUser, function(req, res, next) {
   res.render('users/index')
 });
 
@@ -13,8 +17,12 @@ router.get('/:someUser/edit', function(req, res, next) {
   res.render('users/edit', {user: req.params.someUser})
 });
 
-router.get('/:user/profile', function(req, res, next) {
-  res.render('users/profile', {profileUser: req.params.user})
-});
+function ensureLoggedInUser (req, res, next) {
+  if (res.locals.currentUser) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
 module.exports = router;
